93390448ba146694b8b635245b856c7e1f2b7566
Juyeong Maing <mjy9088@naver.com>
2023-04-18 12:27:13 +0900

    fix norm error

--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -28,6 +28,12 @@ typedef t_err										(*t_a)(
 	char **out
 );
 
+typedef struct s_l
+{
+	t_err	r;
+	char	*p;
+}	t_l;
+
 typedef void										*(*t_m)(size_t size);
 
 static const t_get_next_line_string_util_type		g_0
@@ -98,30 +104,29 @@ t_err	get_next_line_string_alloc_util(
 	char **o
 )
 {
-	t_err	r;
-	char	*p;
+	t_l	l;
 
-	r = false;
-	p = g_n;
-	(t == g_f) && (p = (char *)a);
-	free(p);
+	l.r = false;
+	l.p = g_n;
+	(t == g_f) && (l.p = (char *)a);
+	free(l.p);
 	(t == g_f && b) && (g_a(t, b, g_n, g_n));
-	p = g_n;
-	(t == g_b) && (p = g_m(g_u(a, g_0, 0, g_n) + g_u(b, g_0, 0, g_n) + 1));
-	(t == g_b && p)
-		&& (g_u(a, g_3, 0, p) || g_u(b, g_3, 0, p + g_u(a, g_0, 0, g_n)));
-	(t == g_c) && (p = g_m(g_u(a, g_2, 0, g_n) + 1));
-	(t == g_c && p) && (g_u(a, g_4, 0, p));
-	(t == g_d) && (p = g_m(g_u(a + g_u(a, g_2, 0, g_n), g_0, 0, g_n) + 1));
-	(t == g_d && p) && (g_u(a + g_u(a, g_2, 0, g_n), g_3, 0, p));
-	(t == g_e) && (p = g_m(g_u(a, g_0, 0, g_n) + 1));
-	(t == g_e && p) && (g_u(a, g_3, 0, p));
-	((t == g_b || t == g_c || t == g_d || t == g_e) && !p) && (r = true);
-	(t == g_g) && (p = (char *)a);
-	(t == g_g && g_u("", g_5, 0, (char *)a)) && (r = true);
-	(t == g_g && r) && (g_a(g_f, a, g_n, g_n) || (p = g_n));
+	l.p = g_n;
+	(t == g_b) && (l.p = g_m(g_u(a, g_0, 0, g_n) + g_u(b, g_0, 0, g_n) + 1));
+	(t == g_b && l.p)
+		&& (g_u(a, g_3, 0, l.p) || g_u(b, g_3, 0, l.p + g_u(a, g_0, 0, g_n)));
+	(t == g_c) && (l.p = g_m(g_u(a, g_2, 0, g_n) + 1));
+	(t == g_c && l.p) && (g_u(a, g_4, 0, l.p));
+	(t == g_d) && (l.p = g_m(g_u(a + g_u(a, g_2, 0, g_n), g_0, 0, g_n) + 1));
+	(t == g_d && l.p) && (g_u(a + g_u(a, g_2, 0, g_n), g_3, 0, l.p));
+	(t == g_e) && (l.p = g_m(g_u(a, g_0, 0, g_n) + 1));
+	(t == g_e && l.p) && (g_u(a, g_3, 0, l.p));
+	((t == g_b || t == g_c || t == g_d || t == g_e) && !l.p) && (l.r = true);
+	(t == g_g) && (l.p = (char *)a);
+	(t == g_g && g_u("", g_5, 0, (char *)a)) && (l.r = true);
+	(t == g_g && l.r) && (g_a(g_f, a, g_n, g_n) || (l.p = g_n));
 	((t == g_b || t == g_c || t == g_d || t == g_e || t == g_g) && !o)
-		&& (g_a(g_f, p, g_n, g_n) || (r = true));
-	(o) && (*o = p);
-	return (r && t != g_g);
+		&& (g_a(g_f, l.p, g_n, g_n) || (l.r = true));
+	(o) && (*o = l.p);
+	return (l.r && t != g_g);
 }
